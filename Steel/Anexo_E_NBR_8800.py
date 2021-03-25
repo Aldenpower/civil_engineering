3a129ab2ac9201bdaceee514e55147e9c37f9095
Aldenpower <anderson_afsl@hotmail.com>
2021-03-25 12:03:52 -0300

    Adding buckling reason and critical tension to AxialElasticBucklingForce

--- Steel/Anexo_E_NBR_8800.py
+++ Steel/Anexo_E_NBR_8800.py
@@ -7,26 +7,57 @@ class AxialElasticBucklingForce:
         self.profile_ = profile
         self.buckling_ = buckling
         self.case_ = case
-    def Nex(self):
+    def Nex(self, report = False):
         if self.case_ == 'E.1.1':
             Nex = ((pi ** 2) * (self.steel_.E_ * 0.1) * self.profile_.ix_ /
             ((self.buckling_.kxlx_) ** 2))
 
-            return Nex
+            if report:
+                buckling_reason = self.buckling_.kxlx_ / self.profile_.rx_
+
+                #kN/cm2
+                tension = ((pi ** 2) * (self.steel_.E_ * 0.1) /
+                (buckling_reason ** 2))
+
+                print('Buckling reason', round(buckling_reason, 2),
+                'Tension', round(tension, 2), sep = '\n')
+
+            #kN
+            return round(Nex, 2)
+
     
-    def Ney(self):
+    def Ney(self, report = False):
         if self.case_ == 'E.1.1':
             Ney = ((pi ** 2) * (self.steel_.E_ * 0.1) * self.profile_.iy_ /
             ((self.buckling_.kyly_) ** 2))
 
-            return Ney
+            if report:
+                buckling_reason = self.buckling_.kyly_ / self.profile_.ry_
 
-    def Nez(self):
+                tension = ((pi ** 2) * (self.steel_.E_ * 0.1) /
+                (buckling_reason ** 2))
+
+                print('Buckling reason', round(buckling_reason, 2),
+                'Tension', round(tension, 2), sep = '\n')
+
+            #kN
+            return round(Ney, 2)
+
+    def Nez(self, report = False):
         if self.case_ == 'E.1.1':
-            ro = (((self.profile_.rx_) ** 2) + ((self.profile_.ry_) ** 2)) ** 0.5
+            ro = (((self.profile_.rz_) ** 2) + ((self.profile_.rz_) ** 2)) ** 0.5
 
             Nez = (1 / (ro ** 2)) * (((pi ** 2) * self.steel_.E_ * 0.1 *
             self.profile_.cw_) / ((self.buckling_.kzlz_) ** 2) + 
             (self.steel_.G_ * self.profile_.it_))
 
-            return Nez
+            if report:
+                buckling_reason = self.buckling_.kzlz_ / self.profile_.rz_
+
+                tension = ((pi ** 2) * (self.steel_.E_ * 0.1) /
+                (buckling_reason ** 2))
+
+                print('Buckling reason', round(buckling_reason, 2),
+                'Tension', round(tension, 2), sep = '\n')
+            #kN
+            return round(Nez, 2)
