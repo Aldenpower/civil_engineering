0ddc44980b67e2ce9216e10bf04f46ec797b018f
Aldenpower <anderson_afsl@hotmail.com>
2021-03-26 11:33:51 -0300

    Adding table E.1 to buckling

--- Steel/buckling_classes.py
+++ Steel/buckling_classes.py
@@ -3,6 +3,16 @@ class Buckling:
     Buckling
 
     """
+
+    tabela_E_1 = {
+        'a' : (0.5, 0.65),
+        'b' : (0.7, 0.8),
+        'c' : (1, 1.2),
+        'd' : (1, 1),
+        'e' : (2, 2.1),
+        'f' : (2, 2),
+    }
+
     def __init__(self, kx, lx, ky, ly, kz, lz):
         self.kx_ = kx
         self.lx_ = lx
@@ -26,3 +36,4 @@ if __name__ == '__main__':
 
     print(b.kxlx_)
     print(b.kyly_)
+    print(b.tabela_E_1)
